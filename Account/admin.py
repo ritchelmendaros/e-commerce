7561d680f512c7f0e904e03dbbd039cca69bff02
herald-noel <bontilao.heraldnoel@gmail.com>
2023-09-30 00:13:41 +0800

    Show models in admin

--- Account/admin.py
+++ Account/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Customer)
+admin.site.register(Supplier)
+admin.site.register(Courier)
+admin.site.register(Address)
+
+
