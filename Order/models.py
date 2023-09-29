2947e548c896d6984d663285a11234eda66e1810
fronald143 <120772118+fronald143@users.noreply.github.com>
2023-09-29 23:01:12 +0800

    Order model

--- Order/models.py
+++ Order/models.py
@@ -1,8 +1,27 @@
 from django.db import models
-
-class OrderItem(models.Model):
-    pass
+from Account.models import Courier
+from Payment.models import Payment
+from Review.models import Review
 
 
 class Order(models.Model):
-    pass
+    order_id = models.BigAutoField(primary_key=True)
+    courier_id = models.ForeignKey(Courier, on_delete=models.SET_NULL)
+    payment_id = models.ForeignKey(Payment, on_delete=models.SET_NULL)
+    choices = [
+        ("PR", "Preparing Shipment"),
+        ("PE", "Pending"),
+        ("DE", "Delivered"),
+    ]
+    status = models.CharField(max_length=2, choices=choices)
+
+
+class OrderItem(models.Model):
+    order_item_id = models.BigAutoField(primary_key=True)
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
+    review_id = models.ForeignKey(Review, on_delete=models.SET_NULL)
+
+
+
