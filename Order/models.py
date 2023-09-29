867059c8f9fe6cd4553c06c232640dddc316d348
herald-noel <bontilao.heraldnoel@gmail.com>
2023-09-30 00:03:52 +0800

    Fix null foreign key

--- Order/models.py
+++ Order/models.py
@@ -6,8 +6,8 @@ from Review.models import Review
 
 class Order(models.Model):
     order_id = models.BigAutoField(primary_key=True)
-    courier_id = models.ForeignKey(Courier, on_delete=models.SET_NULL)
-    payment_id = models.ForeignKey(Payment, on_delete=models.SET_NULL)
+    courier_id = models.ForeignKey(Courier, models.SET_NULL, null=True)
+    payment_id = models.ForeignKey(Payment, models.SET_NULL, null=True)
     choices = [
         ("PR", "Preparing Shipment"),
         ("PE", "Pending"),
@@ -21,7 +21,7 @@ class OrderItem(models.Model):
     order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=0)
     price = models.IntegerField(default=0)
-    review_id = models.ForeignKey(Review, on_delete=models.SET_NULL)
+    review_id = models.ForeignKey(Review, models.SET_NULL, null=True)
 
 
 
