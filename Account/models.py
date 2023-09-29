e8fe314daa080fc6586055256db06795539dc90a
herald <bontilao.heraldnoel@gmail.com>
2023-09-29 14:52:55 +0800

    Add account models

--- Account/models.py
+++ Account/models.py
@@ -2,17 +2,31 @@ from django.db import models
 
 
 class User(models.Model):
-    pass
+    user_id = models.BigAutoField(primary_key=True)
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    email = models.EmailField()
+    mobile_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
 
 
 class Supplier(User):
-    pass
+    company_name = models.CharField(max_length=50)
 
 
 class Customer(User):
-    pass
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+
+
+class Address(models.Model):
+    address_id = models.BigAutoField()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    address = models.CharField(max_length=50)
 
 
 class Courier(User):
-    pass
+    courier_name = models.CharField(max_length=50)
+    fee = models.FloatField()
 
