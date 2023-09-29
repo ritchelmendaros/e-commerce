719c7366c289a20cdd49632e7c8c4c4169f01465
herald-noel <bontilao.heraldnoel@gmail.com>
2023-09-29 22:29:18 +0800

    Create payment model

--- Payment/models.py
+++ Payment/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from Account.models import Customer
 
 
 class Payment(models.Model):
-    pass
+    payment_id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    amount = models.FloatField()
+    payment_date = models.DateField()
