5762165ad6e2e33e5c0b3622a5ccc2fc5a6d9096
herald-noel <bontilao.heraldnoel@gmail.com>
2023-09-29 23:17:29 +0800

    Fix cycle import

--- Product/models.py
+++ Product/models.py
@@ -1,23 +1,25 @@
 from django.db import models
-from Account.models import User
+from Account.models import Supplier, Customer
 from Order.models import OrderItem
+
 
 class Category(models.Model):
     category_id = models.BigAutoField(primary_key=True)
     category_name = models.CharField(max_length=50)
 
+
 class Product(models.Model):
     product_id = models.BigAutoField(primary_key=True)
     category_id = models.ForeignKey(Category, on_delete=models.SET_NULL)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(Supplier, on_delete=models.CASCADE)
     order_item = models.ForeignKey(OrderItem, on_delete=models.SET_NULL)
     product_name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
     price = models.FloatField(default=0)
     quantity = models.IntegerField(default=0)
 
+
 class Wishlist(models.Model):
     wishlist_id = models.BigAutoField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-
