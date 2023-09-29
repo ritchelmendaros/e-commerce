8e243264770020b7caabe35ba22234a647874004
harknoel <bontilao.harknoel@gmail.com>
2023-09-29 22:34:36 +0800

    Add Product models

--- Product/models.py
+++ Product/models.py
@@ -1,9 +1,23 @@
 from django.db import models
-
-
-class Product(models.Model):
-    pass
-
+from Account.models import User
 
 class Category(models.Model):
-    pass
+    category_id = models.BigAutoField(primary_key=True)
+    category_name = models.CharField(max_length=50)
+
+class Product(models.Model):
+    category_id = models.ForeignKey(Category)
+    user_id = models.ForeignKey(User)
+    order_item = models.ForeignKey(order_item)
+
+    product_id = models.BigAutoField(primary_key=True)
+    product_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+
+class Wishlist(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    wishlist_id = models.BigAutoField(primary_key=True)
