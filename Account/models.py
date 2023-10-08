cbd30ac87b814737b3d4b4037653b9ab14a02c7d
ritchelmendaros <ritchel.mendaros@cit.edu>
2023-10-09 01:44:24 +0800

    add type to User model

--- Account/models.py
+++ Account/models.py
@@ -9,6 +9,7 @@ class User(models.Model):
     email = models.EmailField()
     mobile_number = models.CharField(max_length=20)
     user_address = models.CharField(max_length=50)
+    type = models.CharField(max_length=2)
 
 
 class Supplier(User):
