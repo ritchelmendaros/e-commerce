42f985323e1978501977860184197846641bc6e3
ritchelmendaros <ritchel.mendaros@cit.edu>
2023-10-09 00:11:21 +0800

    defined the form class

---
+++ Ticket/forms.py
@@ -0,0 +1,17 @@
+from django.forms import ModelForm
+from django import forms
+from Account.models import CustomerSupport
+
+
+class CustomerSupportForm(ModelForm):
+    user_id = forms.CharField(widget=forms.TextInput)
+    username = forms.CharField(widget=forms.TextInput)
+    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.CharField(widget=forms.EmailField)
+    mobile_number = forms.CharField(widget=forms.TextInput)
+    address = forms.CharField(widget=forms.TextInput)
+
+    class Meta:
+        model=CustomerSupport
+        fields=['user_id', 'username','password','email','mobile_number', 'address']
+
