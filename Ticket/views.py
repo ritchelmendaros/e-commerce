b66c80ca7e7f3513771a8c3d1f6f60000928f057
ritchelmendaros <ritchel.mendaros@cit.edu>
2023-10-09 00:49:58 +0800

    modified Ticket/views.py

--- Ticket/views.py
+++ Ticket/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, redirect
-from .forms import CustomerSupportForm
+from .forms import CustomerSupportFormReg
 
-
-def CustomerSupportView(request):
+def customer_support_registration_view(request):
     if request.method == 'POST':
-        form = CustomerSupportForm(request.POST)
+        form = CustomerSupportFormReg(request.POST)
         if form.is_valid():
             form.save()
             return redirect('success_url')
     else:
-        form = CustomerSupportForm()
+        form = CustomerSupportFormReg()
     return render(request, 'ticket_template.html', {'form': form})
