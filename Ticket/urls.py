89b45b9e5e69b8dab24d32ba03a809b1e73c83ed
ritchelmendaros <ritchel.mendaros@cit.edu>
2023-10-09 00:53:28 +0800

    modified Ticket\urls.py

--- Ticket/urls.py
+++ Ticket/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import CustomerSupportView
+from .views import customer_support_registration_view
 
 urlpatterns = [
-    path('customer-support/', CustomerSupportView.as_view(), name='customer_support_form'),
+    path('customer-support-registration/', customer_support_registration_view, name='customer_support_registration'),
 ]
