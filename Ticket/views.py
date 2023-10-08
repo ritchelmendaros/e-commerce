4d49ac43a7ad57ee62ba49147468b2b5f1e5dc4e
ritchelmendaros <ritchel.mendaros@cit.edu>
2023-10-09 00:16:19 +0800

    add urls.py

--- Ticket/views.py
+++ Ticket/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomerSupport
 
-# Create your views here.
+def my_view(request):
+    if request.method == 'POST':
+        form = CustomerSupport(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success_url')
+    else:
+        form = CustomerSupport()
+    return render(request, 'ticket_template.html', {'form': form})
