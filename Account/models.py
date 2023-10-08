12398cba0132f40af5fb9dee071ccf53de4713b6
ritchelmendaros <ritchel.mendaros@cit.edu>
2023-10-09 01:48:29 +0800

    specified the default for type

--- Account/models.py
+++ Account/models.py
@@ -9,7 +9,7 @@ class User(models.Model):
     email = models.EmailField()
     mobile_number = models.CharField(max_length=20)
     user_address = models.CharField(max_length=50)
-    type = models.CharField(max_length=2)
+    type = models.CharField(max_length=2, default="CS")
 
 
 class Supplier(User):
