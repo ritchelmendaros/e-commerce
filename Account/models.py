c62220aea25f4c90d4047e7107023cd76403d25b
ritchelmendaros <ritchel.mendaros@cit.edu>
2023-10-11 00:03:36 +0800

    refactor user for django auth users

--- Account/models.py
+++ Account/models.py
@@ -1,30 +1,40 @@
 from django.db import models
 from Ticket.models import CustomerTicket
+from django.contrib.auth.models import AbstractUser
 
 
-class User(models.Model):
+class User(AbstractUser):
     user_id = models.BigAutoField(primary_key=True)
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    email = models.EmailField()
     mobile_number = models.CharField(max_length=20)
     user_address = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=150, blank=False)
+    last_name = models.CharField(max_length=150, blank=False)
+    email = models.EmailField(blank=False)
+
     type_choices = [
         ('CS', 'Customer Support'),
         ('S', 'Supplier'),
         ('CU', 'Customer'),
         ('CO', 'Courier'),
     ]
-    type = models.CharField(max_length=2, choices=type_choices)
+    user_type = models.CharField(max_length=2, choices=type_choices)
+
+    class Meta:
+        verbose_name = 'User'
 
 
 class Supplier(User):
     company_name = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name = 'Supplier'
+
+
 class Customer(User):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Customer'
 
 
 class Address(models.Model):
@@ -32,13 +42,21 @@ class Address(models.Model):
     customer = models.ForeignKey(Customer, models.SET_NULL, null=True)
     address = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name_plural = "Address"
+
 
 class Courier(User):
     courier_name = models.CharField(max_length=50)
     fee = models.FloatField()
+
+    class Meta:
+        verbose_name = 'Courier'
 
 
 class CustomerSupport(User):
     support_name = models.CharField(max_length=50)
     ticket_id = models.ForeignKey(CustomerTicket, on_delete=models.CASCADE, null=True)
 
+    class Meta:
+        verbose_name = 'Customer Support'
